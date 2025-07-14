5d395766e8a7e82779f670ae461ab32440a0647d
Geeta Bhimsen Maharana <geetabhimsenmaharana@Geetas-MacBook-Air.local>
2025-07-13 21:24:52 -0400

    Initial commit of techgirlprofile portfolio

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,10 @@
 //wait until the DOM is fully loaded
 alert('JavaScript is loaded!');
 
+
+// script.js
+
+// Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header h1');
     header.style.cursor = 'pointer';
